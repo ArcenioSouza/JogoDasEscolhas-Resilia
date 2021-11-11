72404ddf4a3689f1a30b346f5c64d3fde077663a
ArcenioSouza <arcenio_neto@icloud.com>
2021-11-10 22:40:15 -0300

    Adiciona tela inicial ao site

---
+++ index.js
@@ -0,0 +1,15 @@
+var cliente = "Alcides Garcia"
+var conta = 67768734
+var saldo = 322.95
+var deposito = 50.00
+
+console.log('Cliente: '+cliente+'\n'+'Conta: '+conta+'\n'+'Saldo: '+saldo)
+
+saldo = saldo + deposito
+
+console.log('Novo deposito na conta '+conta+'\n'+'Saldo atualizado: '+saldo)
+
+console.log(typeof(cliente))
+console.log(typeof(conta))
+console.log(typeof(saldo))
+console.log(typeof(deposito))
