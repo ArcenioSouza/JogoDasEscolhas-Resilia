166c970e2b865d318bd51f2bea1473d2ccace588
Arcenio Souza <arcenio_neto@icloud.com>
2021-11-18 15:33:58 -0300

    Adiciona logica javascript na historia do Harry

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ function escolhaPersonagem(number) {
   }
 }
 
+function voltar(){
+  return location = ("./index.html")
+}
 
 
 function opcoesJogo(personagem, fase, opcao){
@@ -31,17 +34,43 @@ function opcoesJogo(personagem, fase, opcao){
          if(opcao == 1){
             return location = ("./Fase2.html")
          }else {
-            return location = ("../tela-escolha/index.html")
+            return location = ("../gameOver.html")
          }
 
       }else if(fase == 2){
+        opcao = prompt("Digite o número que corresponde a sua resposta:\n1 para Lily \n2 para Petúnia")
+
+         if(opcao == 1){
+            return location = ("../gameOver.html")
+         }else {            
+            return location = ("./Fase3.html")
+         }
          
       }else if(fase == 3){
+        opcao = prompt("Digite o número que corresponde a sua resposta:\n1 para Atacante \n2 para Apanhador")
+
+         if(opcao == 1){
+            return location = ("../gameOver.html")
+         }else {            
+            return location = ("./Fase4.html")
+         }
 
       }else if(fase == 4){
+        opcao = prompt("Digite o número que corresponde a sua resposta:\n1 para Tom Riddle \n2 para Principe Mestiço")
 
+        if(opcao == 1){
+          return location = ("./Fase5.html")           
+        }else {            
+          return location = ("../gameOver.html")
+        }
    }else{
+      opcao = prompt("Digite o número que corresponde a sua resposta:\n1 para Sirius Black \n2 Alastor Moody")
 
+      if(opcao == 1){
+        return location = ("../vitoria.html")        
+      }else {            
+        return location = ("../gameOver.html")
+      }
    }
 
    }else if(personagem == draco){
