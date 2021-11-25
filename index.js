a5d7a64cb162c614fdf11fac7f22941343601939
Arcenio Souza <arcenio_neto@icloud.com>
2021-11-25 17:16:20 -0300

    Atualiza com laços de repetição nos personagens draco e voldemort

--- index.js
+++ index.js
@@ -19,12 +19,13 @@ function voltar() {
 function opcoesJogo() {}
 
 function opcoesJogo(personagem, fase, opcao) {
+
   //condições do personagem harry
   if (personagem == "harry") {
+
     //codições da fase 0
     if (fase == 0) {
       var valida = false;
-
       while (valida == false) {
         opcao = prompt("Digite a opção:\n1 para sim\n2 para não");
         if (opcao == 1) {
@@ -37,10 +38,10 @@ function opcoesJogo(personagem, fase, opcao) {
           alert("Opção não disponível");
         }
       }
+
       //codições da fase 1
     } else if (fase == 1) {
       valida = false;
-
       while (valida == false) {
         opcao = prompt(
           "Digite o número que corresponde a sua resposta:\n1 para 1980\n2 para 1982"
@@ -56,10 +57,10 @@ function opcoesJogo(personagem, fase, opcao) {
           alert("Opção não disponível");
         }
       }
+
       //condições da fase 2
     } else if (fase == 2) {
       valida = false;
-
       while (valida == false) {
         opcao = prompt(
           "Digite o número que corresponde a sua resposta:\n1 para Lily \n2 para Petúnia"
@@ -75,10 +76,10 @@ function opcoesJogo(personagem, fase, opcao) {
           alert("Opção não disponível");
         }
       }
+
       // condições da fase 3
     } else if (fase == 3) {
       valida = false;
-
       while (valida == false) {
         opcao = prompt(
           "Digite o número que corresponde a sua resposta:\n1 para Atacante \n2 para Apanhador"
@@ -94,6 +95,7 @@ function opcoesJogo(personagem, fase, opcao) {
           alert("Opção não disponível");
         }
       }
+
       //condições da fase 4
     } else if (fase == 4) {
       valida = false;
@@ -112,6 +114,7 @@ function opcoesJogo(personagem, fase, opcao) {
           alert("Opção não disponível");
         }
       }
+
       //condições da fase 5
     } else {
       valida = false;
@@ -134,72 +137,117 @@ function opcoesJogo(personagem, fase, opcao) {
 
     //condições do personagem draco
   } else if (personagem == "draco") {
-    //condições da fase 1
+
+    //condições da fase 0
     if (fase == 0) {
-      opcao = prompt("Digite a opção:\n1 para sim\n2 para não");
+      valida = false;
+      while (valida == false) {
+        opcao = prompt("Digite a opção:\n1 para sim\n2 para não");
 
-      if (opcao == 1) {
-        return (location = "./Fase1.html");
-      } else {
-        return (location = "../tela-escolha/index.html");
+        if (opcao == 1) {
+          valida = true;
+          return (location = "./Fase1.html");
+        } else if (opcao == 2) {
+          valida = true;
+          return (location = "../tela-escolha/index.html");
+        } else {
+          alert("Opção não disponível");
+        }
       }
-    } else if (fase == 1) {
-      opcao = prompt(
-        "Digite o número que corresponde a sua resposta:\n1 para Narcisa\n2 para Ninfadora"
-      );
 
-      if (opcao == 1) {
-        return (location = "./Fase2.html");
-      } else {
-        return (location = "../gameOver.html");
+      //condições da fase 1
+    } else if (fase == 1) {
+      valida = false;
+      while (valida == false) {
+        opcao = prompt(
+          "Digite o número que corresponde a sua resposta:\n1 para Narcisa\n2 para Ninfadora"
+        );
+
+        if (opcao == 1) {
+          valida = true;
+          return (location = "./Fase2.html");
+        } else if (opcao == 2) {
+          valida = true;
+          return (location = "../gameOver.html");
+        } else {
+          alert("Opção não disponível");
+        }
       }
 
       //condições da fase 2
     } else if (fase == 2) {
-      opcao = prompt(
-        "Digite o número que corresponde a sua resposta:\n1 para Neville \n2 para Vicente"
-      );
+      valida = false;
+      while (valida == false) {
+        opcao = prompt(
+          "Digite o número que corresponde a sua resposta:\n1 para Neville \n2 para Vicente"
+        );
 
-      if (opcao == 1) {
-        return (location = "../gameOver.html");
-      } else {
-        return (location = "./Fase3.html");
+        if (opcao == 1) {
+          valida = true;
+          return (location = "../gameOver.html");
+        } else if (opcao == 2) {
+          valida = true;
+          return (location = "./Fase3.html");
+        } else {
+          alert("Opção não disponível");
+        }
       }
 
       //condições da fase 3
     } else if (fase == 3) {
-      opcao = prompt(
-        "Digite o número que corresponde a sua resposta:\n1 Insultou os pais dele \n2 Conjurou uma cobra"
-      );
+      valida = false;
+      while (valida == false) {
+        opcao = prompt(
+          "Digite o número que corresponde a sua resposta:\n1 Insultou os pais dele \n2 Conjurou uma cobra"
+        );
 
-      if (opcao == 1) {
-        return (location = "../gameOver.html");
-      } else {
-        return (location = "./Fase4.html");
+        if (opcao == 1) {
+          valida = true;
+          return (location = "../gameOver.html");
+        } else if (opcao == 2) {
+          valida = true;
+          return (location = "./Fase4.html");
+        } else {
+          alert("Opção não disponível");
+        }
       }
 
       //condições da fase 4
     } else if (fase == 4) {
-      opcao = prompt(
-        "Digite o número que corresponde a sua resposta:\n1 para 50 pontos \n2 para 100 pontos"
-      );
+      valida = false;
+      while (valida == false) {
+        opcao = prompt(
+          "Digite o número que corresponde a sua resposta:\n1 para 50 pontos \n2 para 100 pontos"
+        );
 
-      if (opcao == 1) {
-        return (location = "./Fase5.html");
-      } else {
-        return (location = "../gameOver.html");
+        if (opcao == 1) {
+          valida = true;
+          return (location = "./Fase5.html");
+        } else if (opcao == 2) {
+          valida = true;
+          return (location = "../gameOver.html");
+        } else {
+          alert("Opção não disponível");
+        }
       }
 
       //condições da fase 5
     } else {
-      opcao = prompt(
-        "Digite o número que corresponde a sua resposta:\n1 para Potter Fede \n2 Criado por trouxas"
-      );
+      valida = false;
+      while (valida == false) {
+        opcao = prompt(
+          "Digite o número que corresponde a sua resposta:\n1 para Potter Fede \n2 Criado por trouxas"
+        );
 
-      if (opcao == 1) {
-        return (location = "../vitoria.html");
-      } else {
-        return (location = "../gameOver.html");
+        if (opcao == 1) {
+          valida = true;
+          return (location = "../vitoria.html");
+        } else if (opcao == 2) {
+          valida = true;
+          return (location = "../gameOver.html");
+        } else {
+          alert("Opção não disponível");
+        }
       }
     }
 
@@ -208,72 +256,114 @@ function opcoesJogo(personagem, fase, opcao) {
 
     //condições da fase 0
     if (fase == 0) {
-      opcao = prompt("Digite a opção:\n1 para sim\n2 para não");
+      valida = false;
+      while (valida == false) {
+        opcao = prompt("Digite a opção:\n1 para sim\n2 para não");
 
-      if (opcao == 1) {
-        return (location = "./Fase1.html");
-      } else {
-        return (location = "../tela-escolha/index.html");
+        if (opcao == 1) {
+          valida = true;
+          return (location = "./Fase1.html");
+        } else if (opcao == 2) {
+          valida = true;
+          return (location = "../tela-escolha/index.html");
+        } else {
+          alert("Opção não disponível");
+        }
       }
 
       //condições da fase 1
     } else if (fase == 1) {
-      opcao = prompt(
-        "Digite o número que corresponde a sua resposta:\n1 para Murta Warren\n2 para Luna Lovegood"
-      );
+      valida = false;
+      while (valida == false) {
+        opcao = prompt(
+          "Digite o número que corresponde a sua resposta:\n1 para Murta Warren\n2 para Luna Lovegood"
+        );
 
-      if (opcao == 1) {
-        return (location = "./Fase2.html");
-      } else {
-        return (location = "../gameOver.html");
+        if (opcao == 1) {
+          valida = true;
+          return (location = "./Fase2.html");
+        } else if (opcao == 2) {
+          valida = true;
+          return (location = "../gameOver.html");
+        } else {
+          alert("Opção não disponível");
+        }
       }
 
       //condições da fase 2
     } else if (fase == 2) {
-      opcao = prompt(
-        "Digite o número que corresponde a sua resposta:\n1 para 8 partes \n2 para 7 partes"
-      );
+      valida = false;
+      while (valida == false) {
+        opcao = prompt(
+          "Digite o número que corresponde a sua resposta:\n1 para 8 partes \n2 para 7 partes"
+        );
 
-      if (opcao == 1) {
-        return (location = "../gameOver.html");
-      } else {
-        return (location = "./Fase3.html");
+        if (opcao == 1) {
+          valida = true;
+          return (location = "../gameOver.html");
+        } else if (opcao == 2) {
+          valida = true;
+          return (location = "./Fase3.html");
+        } else {
+          alert("Opção não disponível");
+        }
       }
 
       //condições da fase 3
     } else if (fase == 3) {
-      opcao = prompt(
-        "Digite o número que corresponde a sua resposta:\n1 Sangue de servo fiel  | Cabelo do Harry | Ossos de Tom Riddle \n2 Sangue do Harry | Pedaço de servo fiel | Ossos do Pai"
-      );
+      valida = false;
+      while (valida == false) {
+        opcao = prompt(
+          "Digite o número que corresponde a sua resposta:\n1 Sangue de servo fiel  | Cabelo do Harry | Ossos de Tom Riddle \n2 Sangue do Harry | Pedaço de servo fiel | Ossos do Pai"
+        );
 
-      if (opcao == 1) {
-        return (location = "../gameOver.html");
-      } else {
-        return (location = "./Fase4.html");
+        if (opcao == 1) {
+          valida = true;
+          return (location = "../gameOver.html");
+        } else if (opcao == 2) {
+          valida = true;
+          return (location = "./Fase4.html");
+        } else {
+          alert("Opção não disponível");
+        }
       }
 
       //condições da fase 4
     } else if (fase == 4) {
-      opcao = prompt(
-        "Digite o número que corresponde a sua resposta:\n1 para Expelliarmus \n2 para Imperius"
-      );
+      valida = false;
+      while (valida == false) {
+        opcao = prompt(
+          "Digite o número que corresponde a sua resposta:\n1 para Expelliarmus \n2 para Imperius"
+        );
 
-      if (opcao == 1) {
-        return (location = "./Fase5.html");
-      } else {
-        return (location = "../gameOver.html");
+        if (opcao == 1) {
+          valida = true;
+          return (location = "./Fase5.html");
+        } else if (opcao == 2) {
+          valida = true;
+          return (location = "../gameOver.html");
+        } else {
+          alert("Opção não disponível");
+        }
       }
 
       //condições da fase 5
     } else {
-      opcao = prompt(
-        "Digite o número que corresponde a sua resposta:\n1 para Delphini  \n2 para Belatrix"
-      );
+      valida = false;
+      while (valida == false) {
+        opcao = prompt(
+          "Digite o número que corresponde a sua resposta:\n1 para Delphini  \n2 para Belatrix"
+        );
 
-      if (opcao == 1) {
-        return (location = "../vitoria.html");
-      } else {
-        return (location = "../gameOver.html");
+        if (opcao == 1) {
+          valida = true;
+          return (location = "../vitoria.html");
+        } else if (opcao == 2) {
+          valida = true;
+          return (location = "../gameOver.html");
+        } else {
+          alert("Opção não disponível");
+        }
       }
     }
   }
